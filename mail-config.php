6031696cc4d5e2cf255391cd3dd44446ebb9120f
Mustafa Kaan <MustafaKaanYavuz@outlook.com>
2025-08-05 16:17:07 +0300

    Update mail-config.php

--- mail-config.php
+++ mail-config.php
@@ -1,14 +1,14 @@
 <?php
-define('SMTP_HOST', 'mail.kurumsaleposta.com');
-define('SMTP_PORT', 465);
-define('SMTP_USERNAME', 'info@asgeozemin.com');
-define('SMTP_PASSWORD', 'Asgeo20savAS');
-define('SMTP_ENCRYPTION', 'ssl');
-define('FROM_EMAIL', 'info@asgeozemin.com');
-define('FROM_NAME', 'AS GEO ZEMİN');
-define('TO_EMAIL', 'info@asgeozemin.com');
-define('TO_NAME', 'AS GEO ZEMİN Yönetim');
-define('SITE_NAME', 'AS GEO ZEMİN');
-define('SITE_URL', 'https://asgeozemin.com');
-define('MAIL_DEBUG', false);
+define('SMTP_HOST', '########');
+define('SMTP_PORT', ########);
+define('SMTP_USERNAME', '########');
+define('SMTP_PASSWORD', '########');
+define('SMTP_ENCRYPTION', '########');
+define('FROM_EMAIL', '########');
+define('FROM_NAME', '########');
+define('TO_EMAIL', '########');
+define('TO_NAME', '########');
+define('SITE_NAME', '########');
+define('SITE_URL', '########');
+define('MAIL_DEBUG',########);
 ?>
